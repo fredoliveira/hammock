ed5b9176283837b88d50fb800f1e12694c2fba7f
Fred Oliveira <fred@helloform.com>
2014-03-17 20:49:45 +0000

    Added processing for new desk.com cases

--- plugins/desk/plugin.php
+++ plugins/desk/plugin.php
@@ -8,34 +8,89 @@ class desk extends SlackServicePlugin {
   );
 
   function onView(){
-    return $this->smarty->fetch('view.txt');
+    return $this->smarty->fetch('view.html');
   }
 
+  function onEdit(){
+    $channels = $this->getChannelsList();
+
+    if ($_GET['save']){
+        $this->icfg['channel'] = $_POST['channel'];
+        $this->icfg['channel_name'] = $channels[$_POST['channel']];
+        $this->icfg['botname'] = $_POST['botname'];
+        $this->saveConfig();
+
+        header("location: {$this->getViewUrl()}&saved=1");
+        exit;
+    }
+
+    $this->smarty->assign('channels', $channels);
+    return $this->smarty->fetch('edit.html');
+  }
+
+  # GET vars      : $req['get']
+  # POST vars     : $req['post']
+  # Raw POST body : $req['post_body']
+  # HTTP headers  : $req['headers']
+
   function onHook($req){
-    # GET vars      : $req['get']
-    # POST vars     : $req['post']
-    # Raw POST body : $req['post_body']
-    # HTTP headers  : $req['headers']
+    if (!$this->icfg['channel']){
+      return array(
+        'ok'  => false,
+        'error' => "No channel configured",
+      );
+    }
 
-    # Message the POST vars
-    $this->postToChannel($req['post'], array(
-      'channel' => 'C0260JLAX',
-      'channel_name' => '#tests',
-      'username' => 'Desk',
+    # Desk, being not so smart, always posts a payload in the 'data'
+    # param. Slack, on the other hand, expects that payload to be posted
+    # in a variable called 'payload'. So we have to dissect it here.
+    $desk_payload = json_decode($req['post']['data'], true);
+
+    # Abort if we didn't receive a payload, or if we couldn't decode it.
+    if (!$desk_payload || !is_array($desk_payload)){
+      return array(
+        'ok'  => false,
+        'error' => "No payload received from Desk.com",
+      );
+    }
+
+    if($desk_payload['type'] == "new_case") {
+      $text .= $this->escapeText("#{$desk_payload['id']}: ");
+      $text .= $this->escapeLink($desk_payload['url'], "{$desk_payload['subject']}");
+      $text .= $this->escapeText(" opened by {$desk_payload['user']}");
+
+      # A more complicated version using attachments would
+      # look something like this:
+      #
+      # $this->postToChannel($text, array(
+      #   'channel'       => $this->icfg['channel'],
+      #   'username'      => $this->icfg['botname'],
+      #   'attachments'   => array(
+      #     array(
+      #       "text" => "{$desk_payload['description']}",
+      #       "color" => "#999999",
+      #     ),
+      #   )
+      # ));
+
+      return $this->sendMessage($text);
+    }
+
+    return array(
+      'ok'    => true,
+      'status'  => "Couldn't interpret payload",
+    );
+  }
+
+  private function sendMessage($text, $attachment){
+    $ret = $this->postToChannel($text, array(
+      'channel'       => $this->icfg['channel'],
+      'username'      => $this->icfg['botname'],
     ));
 
-    # Message the POST body
-    $this->postToChannel($req['post_body'], array(
-      'channel' => 'C0260JLAX',
-      'channel_name' => '#tests',
-      'username' => 'Desk',
-    ));
-
-    # Message the actual headers
-    $this->postToChannel($req['headers'], array(
-      'channel' => 'C0260JLAX',
-      'channel_name' => '#tests',
-      'username' => 'Desk',
-    ));
+    return array(
+      'ok'        => true,
+      'status'    => "Sent a message",
+    );
   }
 }
